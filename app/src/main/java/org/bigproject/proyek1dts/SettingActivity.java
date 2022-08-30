d888eac174fe37ffc05d40d8b958ae388142846e
muzakidian <muzakidian1@gmail.com>
2022-08-30 09:30:23 +0700

    puzzle

---
+++ app/src/main/java/org/bigproject/proyek1dts/SettingActivity.java
@@ -0,0 +1,14 @@
+package org.bigproject.proyek1dts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
